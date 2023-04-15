96b8dd020a94fbf38bfc7ecc4e59953b0576030c
chiche <chides2012@gmail.com>
2023-04-15 01:31:29 +0100

    add templetes

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
+
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    return render(request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse('<h1> Blog About </h1>')
+    return render(request, 'blog/about.html')
 
 def contact(request):
     return HttpResponse('<h1> Blog contact </h1>')
