19fcff9e9c6068de2c02a0aa8ede7bc07f8c6865
chiche <chides2012@gmail.com>
2023-04-15 03:15:20 +0100

    setup base templete

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,32 @@ from django.shortcuts import render
 
 # Create your views here.
 
+posts =[
+    {
+    'author':'CoreyMs',
+    'title':'blog psot 1',
+    'content': 'first post content',
+    'date_posted':'april 15, 2022'
+
+    },
+    {
+    'author':'jane Doe',
+    'title':'blog psot 2',
+    'content': 'second post content',
+    'date_posted':'april 12, 2022'
+
+    }
+
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    content = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html',content)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html',{'title':'About'})
 
 def contact(request):
     return HttpResponse('<h1> Blog contact </h1>')
