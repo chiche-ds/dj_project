dd8746c925c744286ea0077ec84c6a3832acaeec
chiche <chides2012@gmail.com>
2023-04-17 16:31:08 +0100

    creating models

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-
-posts =[
-    {
-    'author':'CoreyMs',
-    'title':'blog psot 1',
-    'content': 'first post content',
-    'date_posted':'april 15, 2022'
-
-    },
-    {
-    'author':'jane Doe',
-    'title':'blog psot 2',
-    'content': 'second post content',
-    'date_posted':'april 12, 2022'
-
-    }
-
-]
-
 def home(request):
     content = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html',content)
 
